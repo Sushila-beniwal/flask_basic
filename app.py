842f8c236a05d015405acc1c1a511df8c72d5300
sushila <beniwalsushila963@gmail.com>
2025-04-11 13:02:09 +0530

    Update app.py
    i have added some more basic info about flask i.e. render_template,url_for and redirect

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>"
+
+@app.route("/passed/<name>/<int:marks>")
+def passed(name,marks):
+    return render_template("passe.html",title="result")
+
+
+@app.route("/passed/<name>/<int:marks>")
+def fail(name,marks):
+    return render_template("fail.html",title="result")
+
+@app.route("/result/<sname>/<int:mark>")
+def result(sname,mark):
+    if mark<30:
+        return redirect("fail",name=sname,marks=mark)
+    else:
+        return redirect("passed",name=sname,marks=mark)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
